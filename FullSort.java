2182b2b9ae89c7f787c2338278c08068a3933b1e
404notfound <2811832487@qq.com>
2019-04-29 16:42:25 +0800

    分治：全排列问题

---
+++ FullSort.java
@@ -0,0 +1,28 @@
+//分治思想下的全排列问题，来源：https://mp.weixin.qq.com/s/paOrlfpdMwvCUDywda0EvQ
+public class FullSort {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4 };
+        fullSort(arr, 0, arr.length - 1);
+    }
+    public static void fullSort(int[] arr, int start, int end) {
+        // 递归终止条件
+        if (start == end) {
+            for (int i : arr) {
+                System.out.print(i);
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i <= end; i++) {
+            swap(arr, i, start);
+            fullSort(arr, start + 1, end);
+            swap(arr, i, start);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
